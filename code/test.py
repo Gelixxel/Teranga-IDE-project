8f727bfe6a3d3a22535fbc5222948b1bee5af195
thomas.florion <thomas.florion@epita.fr>
2024-06-26 21:43:16 +0200

    feat: test avec emojis pour faire plaisir a lamine diop

--- code/test.py
+++ code/test.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def test():
     print(np.array([1, 2, 3, 4, 5, 6]))
-
+❤️
 test()
 
 # j'aime pas les tests
--- code/test2.py
+++ code/test2.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+# c'est le test 2 🤌
 def test():
     print(np.array([1, 2, 3, 4, 5, 6]))
 
